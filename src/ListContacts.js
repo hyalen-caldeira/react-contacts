bccb2cd457beecb4d8e1792c9839a0b7c5c0e796
Moreira <hmoreira@WL-HMOREIR-MAC.local>
2017-08-18 20:07:13 -0700

    Add state to the component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,19 +1,60 @@
 import React, {Component} from 'react';
 
+function ListContacts(props) {
+	return (
+		<ol className='contact-list'>
+			{props.contacts.map((contact) => (
+				<li key={contact.id} className='contact-list-item'>
+					<div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}}>
+					</div>
+					<div className='contact-details'>
+						<p>{contact.name}</p>
+						<p>{contact.email}</p>
+					</div>
+					<button className='contact-remove'>
+						Remove
+					</button>
+				</li>
+			))}
+		</ol>
+	)
+}
+
+/*
+// An example of ES6 function with an implicit return
+
+const Email = (props) => (
+  <div>
+    {props.text}
+  </div>
+);
+*/
+
+/*
 class ListContacts extends Component {
 	render() {
 		// console.log('Props', this.props);
 
-		return <ol className='contact-list'>
-
+		return (
+			<ol className='contact-list'>
 				{this.props.contacts.map((contact) => (
-					<li key={contact.id}>
-						{contact.name}
+					<li key={contact.id} className='contact-list-item'>
+						<div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}}>
+						</div>
+						<div className='contact-details'>
+							<p>{contact.name}</p>
+							<p>{contact.email}</p>
+						</div>
+						<button className='contact-remove'>
+							Remove
+						</button>
 					</li>
-				))}			
-		</ol>
-
+				))}
+			</ol>
+		)
 	}
 }
+*/
+
 
 export default ListContacts;
